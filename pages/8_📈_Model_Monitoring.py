6b2aca70cf384f6fe8cb735968cae8b787c3f240
Bill0210814 <bill0210814@gmail.com>
2026-06-16 21:14:06 +0800

    Update 8_📈_Model_Monitoring.py

--- pages/8_📈_Model_Monitoring.py
+++ pages/8_📈_Model_Monitoring.py
@@ -212,7 +212,6 @@ with col3:
         y="Score",
         text_auto=".3f",
         color="Metric",
-        color_discrete_sequence=["#8c8c8c", "#8EBA42"], # 灰色代表基準，綠色代表你們的模型
         title="Average Precision vs Baseline"
     )
 
