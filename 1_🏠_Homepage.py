b1e14e123d31520c1f513d04f87219838ee3f689
Bill0210814 <bill0210814@gmail.com>
2026-06-17 22:17:56 +0800

    Update 1_🏠_Homepage.py

--- 1_🏠_Homepage.py
+++ 1_🏠_Homepage.py
@@ -20,7 +20,7 @@ st.title("🏦 AI-Powered Credit Risk & Default Intelligence System")
 st.markdown("""
 ### 📊DSF504 The Practice of Big Data and Analysis in the Financial Industry
 
-**TEAM 8** 
+## **TEAM 8** 
 * M14B020009_Bill Lai_賴柏諺  
 * M14B020026_Darryl Lin_林詰寶  
 * M14B020045_Jerry Kuo_郭翔鈞  
